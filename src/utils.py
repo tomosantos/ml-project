e53f2c3a157765f7436ae1b4cdf97db78bc43276
Wellinton Oliveira <wellintonoliveira04@gmail.com>
2025-07-02 21:10:01 -0300

    update utils + add hyp tuning

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import dill
 from src.exception import CustomException
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -21,13 +22,18 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = dict()
         
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, param, cv=3)
+            gs.fit(X_train, y_train)
             
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)     # Train Model
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
